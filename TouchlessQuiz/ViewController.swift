ec0d1219393543e8033dc4f1acdf9e7290da85b2
Can Yoldas <can.yoldas0@outlook.com>
2021-10-05 00:37:55 +0300

    Initial Commit

---
+++ TouchlessQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouchlessQuiz
+//
+//  Created by Can Yoldas on 5.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
